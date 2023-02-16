96c3d615b2f4f0802ff9b76eb44d4b43db34d973
Carlo Jimenez <jimenez.carlo.llabor@gmail.com>
2023-02-16 11:06:15 +0800

    update

--- member/home.php
+++ member/home.php
@@ -1,21 +1,10 @@
 <?php include('header.php'); ?>
+
 <style>
-  .card {
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    transition: 0.3s;
-    width: 40%;
-  }
-
-  .card:hover {
-    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-  }
-
-  .container {
-    padding: 2px 16px;
-    margin-top: 50px;
+  .carousel-img {
+    object-fit: fill;
   }
 </style>
-
 <div>
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
@@ -25,30 +14,192 @@
       </div>
       <div class="container-fluid" id="content">
         <div class="row mb-2">
+
           <div class="col-sm-12">
-            <h1 class="m-0"><i class="fa fa-home"></i> Home</h1>
-
-            
-</section>
-  <section class="container mb-5">
-    <div class="row">
-      <?php foreach (get_list("SELECT * from tbl_services where  deleted_flag = 0 limit 4") as $res) { ?>
-
-        <div class="col">
-          <div class="card" style="width: 18rem;">
-            <img src="../services/<?= $res['image'] ?>" class="card-img-top" alt="..." style="height:286px;width:286px;object-fit:contain">
-            <div class="card-body">
-              <h5 class="card-title"><?= $res['name'] ?></h5>
-     
-            </div>
-    
+            <h1 class="m-0 text-center">Strong Tower Gym</h1><br>
+            <h5 class="m-0 text-center">Strong Tower Gym is the ultimate fitness hub in Urdaneta City. Located at the heart of the bustling</h5>
+            <br>
           </div>
-        </div>
-      <?php } ?>
-    </div>
-  </section>
+          <div class="col-sm-6">
+            <h4 class="m-0 text-center mb-2">Facilities</h4>
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="10"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="11"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="12"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="13"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="14"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="15"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="16"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="17"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="18"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="19"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="20"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="21"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="22"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="23"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="24"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="25"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="26"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="27"></li>
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_1.jpg" alt="First slide" style="height:445px">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_2.jpg" alt="First slide" style="height:445px">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_3.jpg" alt="First slide" style="height:445px">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_4.jpg" alt="First slide" style="height:445px">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_5.jpg" alt="First slide" style="height:445px">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_6.jpg" alt="First slide" style="height:445px">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_7.jpg" alt="First slide" style="height:445px">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_8.jpg" alt="First slide" style="height:445px">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_9.jpg" alt="First slide" style="height:445px">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_10.jpg" alt="First slide" style="height:445px">
+                </div>
+
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_11.jpg" alt="First slide" style="height:445px">
+                </div>
+
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_12.jpg" alt="First slide" style="height:445px">
+                </div>
+
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_13.jpg" alt="First slide" style="height:445px">
+                </div>
+
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_14.jpg" alt="First slide" style="height:445px">
+                </div>
+
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_15.jpg" alt="First slide" style="height:445px">
+                </div>
+
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_16.jpg" alt="First slide" style="height:445px">
+                </div>
+
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_17.jpg" alt="First slide" style="height:445px">
+                </div>
+
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_18.jpg" alt="First slide" style="height:445px">
+                </div>
+
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_19.jpg" alt="First slide" style="height:445px">
+                </div>
+
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_20.jpg" alt="First slide" style="height:445px">
+                </div>
+
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_21.jpg" alt="First slide" style="height:445px">
+                </div>
+
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_22.jpg" alt="First slide" style="height:445px">
+                </div>
+
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_23.jpg" alt="First slide" style="height:445px">
+                </div>
+
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_24.jpg" alt="First slide" style="height:445px">
+                </div>
+
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_25.jpg" alt="First slide" style="height:445px">
+                </div>
+
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_26.jpg" alt="First slide" style="height:445px">
+                </div>
+
+                <div class="carousel-item">
+                  <img class="d-block w-100 carousel-img" src="../facilities/_27.jpg" alt="First slide" style="height:445px">
+                </div>
+
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
           </div><!-- /.col -->
-        
+
+          <div class="col-sm-6">
+            <h4 class="m-0 text-center mb-2">Services</h4>
+            <div id="carouselExampleIndicators_2" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <?php $ctr = 0; ?>
+                <?php foreach (get_list("SELECT * from tbl_services where deleted_flag = 0") as $res) { ?>
+                  <li data-target="#carouselExampleIndicators_2" data-slide-to="<?= $ctr ?>" class="<?= ($ctr == 0) ? 'active' : '' ?>"></li>
+                  <?php $ctr++; ?>
+                <?php }  ?>
+              </ol>
+              <div class="carousel-inner">
+                <?php $ctr = 0; ?>
+                <?php foreach (get_list("SELECT * from tbl_services where deleted_flag = 0") as $res) { ?>
+                  <div class="carousel-item <?= ($ctr == 0) ? 'active' : '' ?>">
+                    <img class="d-block w-100 carousel-img" src="../services/<?= $res['image'] ?>" alt="First slide" style="height:445px">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5><?= $res['name'] ?></h5>
+                      <p><?= $res['description'] ?></p>
+                    </div>
+                  </div>
+                  <?php $ctr++; ?>
+                <?php }  ?>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleIndicators_2" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleIndicators_2" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+          </div><!-- /.col -->
+
+
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
@@ -60,7 +211,4 @@
   </div>
   <!-- /.content-wrapper -->
 </div>
-
-
-
 <?php include('footer.php'); ?>
